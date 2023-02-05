bb57e37b2d13c8b19ae3f68754f36bbbb73cbce6
Glei-jihed <glei.jihed@gmail.com>
2023-02-05 09:22:15 +0100

    firs commit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import "./HomeStyles.css";
+
+
+function Home (){
+    return (<>
+    <div className="home">
+    <h1 className="first">Responsive React<i className="fab fa-react"></i> Developed By GLEI JIHED<i class="fa-sharp fa-solid fa-copyright"></i></h1>
+    
+    </div>
+    
+    </>  );
+}
+
+
+export default Home;
---
+++ src/components/MenuData.js
@@ -0,0 +1,40 @@
+export const MenuData = [
+    {
+        title:"home",
+        url:"#",
+        cName:"nav-links",
+        icon:"fa-solid fa-house-user"
+    },
+    {
+        title:"About",
+        url:"#",
+        cName:"nav-links",
+        icon:"fa-solid fa-circle-info "
+    },
+    {
+        title:"Service",
+        url:"#",
+        cName:"nav-links",
+        icon:"fa-solid fa-briefcase"
+    },
+    {
+        title:"Contact",
+        url:"#",
+        cName:"nav-links",
+        icon:"fa-solid fa-address-book"
+    },
+    {
+        title:"Blog",
+        url:"#",
+        cName:"nav-links",
+        icon:"fa-solid fa-book"
+        
+    },
+    {
+        title:"Sign Up",
+        url:"#",
+        cName:"nav-links-mobile",
+        
+    }
+]
+export default MenuData;
